c7671d71697140147491969e8a7ba4bdb12b4a81
Oleksii Hiriavenko <martmarchmartmarch@gmail.com>
2023-11-20 00:44:19 +0200

    rtk task done partial, not tets

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,14 @@
 import ReactDOM from 'react-dom/client';
 import { Provider as ReduxProvider } from 'react-redux';
 import { store } from './redux/store.tsx';
-// import ContextProvider from './components/Context/ContextProvider.tsx';
 import App from './App.tsx';
 import './styles/index.scss';
 import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <ReduxProvider store={store}>
-    {/* <ContextProvider> */}
     <BrowserRouter>
       <App />
     </BrowserRouter>
-    {/* </ContextProvider> */}
   </ReduxProvider>
 );
